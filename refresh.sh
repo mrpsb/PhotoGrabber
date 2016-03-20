751dfc1100f542843d4bddb0c870a736bd976c54
mrpsb <mrpsb@hotmail.com>
2016-03-20 22:51:11 +0000

    Removed cank

--- refresh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python /home/pi/photograbber/photograbber.py
-pkill fbi
-fbi -a -T 2 -t 30 -u -noverbose -d /dev/fb0 /home/pi/Pictures/*
-
--- restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pkill fbi
-fbi -a -T 2 -t 30 -u -noverbose -d /dev/fb0 /home/pi/Pictures/*
-
