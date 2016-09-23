5c57d1f9669d6e3cbf36687f6a706b596aa63526
mrpsb <mrpsb@hotmail.com>
2016-09-23 20:29:35 +0100

    Tidied up album grabbing code so it actually works

--- photograbber.py
+++ photograbber.py
@@ -1,45 +1,60 @@
+#coding: UTF-8
 from imgurpython import ImgurClient
-from imgurkey import *
+import imgurkey
 import urllib
 import os
 
 # Connect to Imgur
-# Grab the first page of most recent images for srcreddits
-# Download (if not already grabbed)
+# Grab the first page of Specified Subreddits
+# Download everything
 
-imgur = ImgurClient(IMGUR_CLIENT_ID,IMGUR_CLIENT_SECRET)
+imgur = ImgurClient(imgurkey.IMGUR_CLIENT_ID,imgurkey.IMGUR_CLIENT_SECRET)
 
 imgs = []
 u = urllib.URLopener()
 
-srcreddits = ('EarthPorn','LavaPorn','AbandonedPorn','F1Porn',
-		'ThingsCutInHalfPorn','SpaceFlightPorn', 'GeekPorn','SpacePorn')
+srcreddits = (
+         'Pics','EarthPorn',
+             'LavaPorn','AbandonedPorn',
+             'SpacePorn'
+             )
 
 for sub in srcreddits:
-	print "Getting Most Recent Images From Subreddit: r/" + sub
-	imgs += imgur.subreddit_gallery(sub, sort='time', window='day', page=0)
-	
+    print "Getting Most Recent Images From Subreddit: r/" + sub
+    imgs += imgur.subreddit_gallery(sub, sort='time', window='day', page=0)
+
 for img in imgs:
 
+    # If an album according to API only cover img available when accessed as gallery
+    # Use API get_album method to grab all the images in the album
+
     if img.is_album == True:
-        for i in img:
-            if i.width > i.height:
-                print "Downloading from album"
-                u = urllib.URLopener()
-                file = i.link.split('/')[-1]
-                if os.path.isfile('/home/pi/Pictures/' + file):
-		    print "File already exists" 
-		else:
-		    print "Downloading " + i.link + " to file " + i.link.split('/')[-1]
-                    u.retrieve(img.link,"/home/pi/Pictures/" + file)
+        print "Getting album " + img.id
+        i = imgur.get_album(img.id)
+
+        for pix in i.images:
+
+            filename = pix['link'].split('/')[-1]
+
+            #print "Album branch, pix['link'] is " + pix['link'] + " file is " + file
+
+            if int(pix['width']) > int(pix['height']):
+                
+                if os.path.isfile('/home/pi/Pictures/' + filename):
+                    print "Album Branch - File already exists"
+                else:
+                    print "Downloading album image " + pix['link'] + " to file " + filename
+                    u.retrieve(pix['link'],"/home/pi/Pictures/" + filename)
+            else:
+                continue
+
+        # Otherwise just grab the image
 
     else:
-        if img.width > img.height:
-            file = img.link.split('/')[-1]
-            if os.path.isfile('/home/pi/Pictures/' + file):
-                print "File already exists: " + img.link
+        if int(img.width) > int(img.height):
+            filename = img.link.split('/')[-1]
+            if os.path.isfile('/home/pi/Pictures/' + filename):
+                print "Image Branch - File already exists: " + filename
             else:
-		u = urllib.URLopener()
-                print "Downloading " + i.link + " to file " + i.link.split('/')[-1]
-                u.retrieve(img.link,"/home/pi/Pictures/" + file)
-
+                print "Downloading " + img.link  + " to file " + filename
+                u.retrieve(img.link,"/home/pi/Pictures/" + filename)
